a27e74ed36c631c81c31766e1dc0f5ef46406ea9
Joey <hobiger.j@gmail.com>
2019-03-20 15:09:43 +0100

    fkt fuer points/lines zaehlen ohne error aber noch mit falschem ergebnis

--- A3/MHT.cpp
+++ A3/MHT.cpp
@@ -24,6 +24,36 @@ void read_in() {
 int main() {
     
     read_in();
-    
-    
+    // groesse rausfinden
+    string character = "-";
+    int count_points = 0;
+    int count_lines = 0;
+
+    for (int i = 0; i < Angabe.length(); i++) {  // 45 = -
+
+        if (Angabe[i] == 46) {
+            count_points += 1;
+        } 
+        if (Angabe[i] == '\n') {
+            count_lines += 1;
+        }
+        
+    }
+    cout << "points: " << count_points << " and lines: " << count_lines << endl;
+    cout << Angabe.length() << endl;
+    cout << Angabe << endl;
+    /*
+    // matrix1 und matrix2 definieren
+    float i;
+    float j;
+    matrix1[][];
+
+    char = 0;
+    while (char != 46) {   // 46 = .
+        continue;
+        char++;
+        if (char == 46) {
+            matrix[0][0] = 
+        }
+    } */
 }
