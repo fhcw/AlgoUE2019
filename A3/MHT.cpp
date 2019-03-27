a99e7c5465695df3025948dbec59a1c5ea518630
Joey <hobiger.j@gmail.com>
2019-03-27 19:48:37 +0100

    nach phils aenderungen zum matrix befuellen

--- A3/MHT.cpp
+++ A3/MHT.cpp
@@ -10,15 +10,15 @@ using namespace std;
 
 string read_in(string filename);
 
-string Angabe = read_in("rmHV_10_12");
+//string Angabe = read_in("rmHV_10_12");
 
-// Klasse Matrix
 class Matrix {
     private:
         int werteanz;
         int spaltenanz;
         int zeilenanz;
-
+        float* matrix_values;
+       
     public:
         int getWerteanz() {
             return werteanz;
@@ -40,6 +40,40 @@ class Matrix {
         void setWerteanz(int wa) {
             werteanz = wa;
         }
+        void setDimensions() {
+            matrix_values = new float[zeilenanz * spaltenanz];
+        }
+        // Funktion vom Typ Matrix, die eine Matrix zurückgibt. hier werden zeilenanz etc berechnet....
+        void dimensions_matrix1(string Angabe) {
+
+            int number_values_matrix1 = 0;
+            int number_rows_matrix1 = 0;
+                
+            for (int k = 0; k < Angabe.length(); k++) {  // 45 = -, 46 = .
+                if (Angabe[k] == 46) {
+                    number_values_matrix1 += 1;
+                } 
+                if (Angabe[k] == '\n') {
+                    number_rows_matrix1 += 1;
+                }
+                if (Angabe[k] == '-') {
+                    break;
+                }
+            }
+            number_rows_matrix1 -= 1;
+            int number_columns_matrix1 = number_values_matrix1 / number_rows_matrix1; // spalten = Werte / Reihen
+            
+            //Matrix m1;
+        
+            //m1.setSpaltenanz(number_columns_matrix1);
+            //m1.setZeilenanz(number_rows_matrix1);
+            //m1.setDimensions();
+            spaltenanz = number_columns_matrix1;
+            zeilenanz = number_rows_matrix1;
+            matrix_values = new float[zeilenanz * spaltenanz];
+
+            //return m1;
+        }
 
 };
 // string eingabe;
@@ -53,46 +87,21 @@ string read_in(string filename) { // string read_in(string filename) {
         buffer << input_file.rdbuf();
         input_file.close();
 
-        return buffer.str();  // return buffer.str();
+        return buffer.str();
        
 }
 
 
-// Funktion vom Typ Matrix, die eine Matrix zurückgibt. hier werden zeilenanz etc berechnet....
-Matrix values_matrix1(string Angabe) {
 
-    int number_values_matrix1 = 0;
-    int number_rows_matrix1 = 0;
-        
-    for (int k = 0; k < Angabe.length(); k++) {  // 45 = -, 46 = .
-        if (Angabe[k] == 46) {
-            number_values_matrix1 += 1;
-        } 
-        if (Angabe[k] == '\n') {
-            number_rows_matrix1 += 1;
-        }
-        if (Angabe[k] == '-') {
-            break;
-        }
-    }
-    number_rows_matrix1 -= 1;
-    int number_columns_matrix1 = number_values_matrix1 / number_rows_matrix1; // spalten = Werte / Reihen
-    
-    Matrix m1;
-    m1.setSpaltenanz(number_columns_matrix1);
-    m1.setZeilenanz(number_rows_matrix1);
 
-    return m1;
-}
-
-Matrix values_matrix2(Matrix m) {
+Matrix dimensions_matrix2(Matrix m) {
     int number_rows_matrix2 = m.getZeilenanz() + 1;
     int number_columns_matrix2 = m.getSpaltenanz() - 1;
 
     Matrix m2;
     m2.setZeilenanz(number_rows_matrix2);
     m2.setSpaltenanz(number_columns_matrix2);
-
+    
     return m2;
 }
 
@@ -100,26 +109,33 @@ Matrix values_matrix2(Matrix m) {
 int main() { 
 
     Matrix m1;
+    m1.dimensions_matrix1(read_in("rmHV_10_12"));
+
+    //cout << m1.getSpaltenanz() << endl;
+    //cout << m1.getZeilenanz();
+    
+
     m1.getSpaltenanz();
     m1.getZeilenanz();    
+    cout << "Matrix 1" << endl;
+    //cout << "Spaltenanz: " << dimensions_matrix1(Angabe).getSpaltenanz() << "und Reihenanz: " << dimensions_matrix1(Angabe).getZeilenanz() << endl;
 
-    cout << "Spaltenanz: " << values_matrix1(Angabe).getSpaltenanz() << "und Reihenanz: " << values_matrix1(Angabe).getZeilenanz() << endl;
 
+    Matrix m2;
+    m2.getSpaltenanz();
+    m2.getZeilenanz();
 
-   
+    cout << "Matrix 2" << endl;
+    cout << "Spalten: " << dimensions_matrix2(m1).getSpaltenanz() << " und Reihen: " << dimensions_matrix2(m1).getZeilenanz() << endl;
   
-       /* 
-
-    // werte, Reihen, Spalten für matrix 2
-    int number_values_matrix2 = number_values_matrix1;
-    int number_rows_matrix2 = number_rows_matrix1 + 1;
-    int number_columns_matrix2 = number_columns_matrix1 - 1;
+       /*
 
     // matrix1 und matrix2 deklarieren
     float matrix1[number_rows_matrix1][number_columns_matrix1];
-    float matrix2[number_rows_matrix2][number_columns_matrix2];
+    float matrix2[number_rows_matrix2][number_columns_matrix2]; 
     
     // matrix 1   funktion draus machen
+
     int cursor = 0;
     for (int y = 0; y < number_rows_matrix1; y++) {
         for (int x = 0; x < number_columns_matrix1; x++) {
@@ -137,7 +153,7 @@ int main() {
             value += ch3;
             value += ch4;
             float float_value = stof(value);
-            matrix1[x][y] = float_value;
+            matrix1[x][y] = float_value;   // m1.setMatrixValues(x, y, float_value);
             cursor ++;   
         }
     } */
