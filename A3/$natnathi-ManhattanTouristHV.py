d6fc6924e2159e99ab8df4e9f1d47931de714ab8
natnathi <nropek@protonmail.com>
2019-04-03 23:52:11 +0200

    corrected A3

--- A3/$natnathi-ManhattanTouristHV.py
+++ A3/$natnathi-ManhattanTouristHV.py
@@ -19,14 +19,14 @@ def manhattan(v, h, rows, columns):           # v = down grid, h = right grid
         grid.append([0] * columns)
 
     grid[0][0] = 0                            # fill first row and first column
-    for i in range(0, rows):
-        grid[i][0] = grid[i - 1][0] + v[i][0]
-    for j in range(0, columns):
-        grid[0][j] = grid[0][j - 1] + h[0][j]
+    for i in range(1, rows):
+        grid[i][0] = grid[i - 1][0] + v[i - 1][0]
+    for j in range(1, columns):
+        grid[0][j] = grid[0][j - 1] + h[0][j - 1]
 
     for i in range(1, rows):                  # fill rest
         for j in range(1, columns):
-            grid[i][j] = max(grid[i - 1][j] + v[i][j], grid[i][j - 1] + h[i][j])
+            grid[i][j] = max(grid[i - 1][j] + v[i - 1][j], grid[i][j - 1] + h[i][j - 1])
 
     print(grid[rows - 1][columns - 1])
 
@@ -51,7 +51,8 @@ def extract_and_calc_grid(user_file):
     g_down = down[1:len(down)-1]               # save g_down but cut out header and ---
     g_right = right[1:len(right)-1]            # save g_right but cut out header and ---
 
-    size_of_grid = len(g_down)                # length of g_right gives correct size of grid
+    size_of_grid = len(g_right)                # length of g_right gives correct size of grid
+
 
     g_down = optimize_grid(g_down)
     g_right = optimize_grid(g_right)
